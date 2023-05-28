e90a0859e373fe751d31749aeecb0cbbbe4f1613
Sharique <syedshariq05@gmail.com>
2023-05-28 19:02:45 +0500

    setup nav bar

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function about() {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur impedit unde maxime quas, hic vitae consequuntur ipsam. Ratione nemo deserunt tempore harum, ipsa amet accusantium sapiente, similique debitis aliquid maxime.
+        </div>
+    )
+}
+
+export default about
